5c3273f74849cdaeb9bb2113f0e5f43efcab6711
Irynka88 <126070772+Irynka88@users.noreply.github.com>
2023-03-29 21:47:23 +0300

    lab3
    lab3

---
+++ src/Main.java
@@ -0,0 +1,84 @@
+public class Main {
+    public static void main(String[] args) {
+      //Task1
+//        int n = 40;
+//        if ((n>25)&&(n<100)){
+//            System.out.println("Число " + n + " міститься в проміжку (25; 100)");}
+//        else{
+//            System.out.println("Число " + n + " не міститься в проміжку (25; 100)");}
+
+        //Task2
+
+//        int n = 188;
+//        int a = n/100;
+//        int b = ((n/10)%10);
+//        int c = ((n%100)%10);
+//        if (a>=b && a>=c){
+//            System.out.println(a);}
+//        else if (b>=a && b>=c) {
+//            System.out.println(b);}
+//        else if (c>=a && c>=b) {
+//            System.out.println(c);}
+
+        //Task3
+//       String n = "До низу";
+//        int a = 8;
+//        if (n.equals("До верху")){
+//            if (a == 2 || a == 3) System.out.println("Ви піднялись на 3 поверх");
+//            else if (a <= 9 && a > 0) System.out.println("Ви піднялись на "+ a +" поверх");
+//            else System.out.println("Ви вказали неіснуючий поверх!");
+//        }
+//        if (n.equals("До низу")){
+//            if (a == 2 || a == 1) System.out.println("Ви спустились на 1 поверх");
+//            else if (a <= 9 && a > 0) System.out.printf("Ви спустились на "+ a +" поверх");
+//            else System.out.println("Ви вказали неіснуючий поверх!");
+//        }
+
+
+       //Task4
+        String option = "-";
+        switch (option) {
+            case "Так":
+            case "ОК":
+            case "Yes":
+            case "Y":
+            case "+":
+            case "Ok":
+                System.out.println("Я погоджуюсь!");
+                break;
+            case "Ні":
+            case "NO":
+            case "N":
+            case "-":
+            case "No":
+                System.out.println("Я відмовляюcь!");
+                break;
+            default:
+                System.out.println("Не існує");}
+
+
+
+
+
+
+
+        }
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
